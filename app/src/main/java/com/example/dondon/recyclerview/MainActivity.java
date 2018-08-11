df371b9a221b1d7effecb786127de6240bed7363
brandondasb <brandonmilambo.dev@gmail.com>
2018-08-11 21:30:46 +0100

    first java

--- app/src/main/java/com/example/dondon/recyclerview/MainActivity.java
+++ app/src/main/java/com/example/dondon/recyclerview/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.dondon.recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.support.v4.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Button StartButton = new Button(this); //try to add new button view Java epic fail
+        Button StartButton = findViewById(R.id.start_button);
+        StartButton.setText(" java created button");
+       // setContentView();
+        StartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getSupportFragmentManager().beginTransaction().commit();
+                FragmentPopup fragmentPopup = new FragmentPopup();
+                //fragmentPopup();// call fragment class
+
+            }
+        });
+
+
+
     }
 }
