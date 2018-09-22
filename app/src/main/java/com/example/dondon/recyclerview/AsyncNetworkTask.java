71eec983b3582dd2aa79c2af8ab940b0b431bf80
brandondasb <42249890+brandondasb@users.noreply.github.com>
2018-09-22 15:18:11 +0100

    fixed the blank screen issue

---
+++ app/src/main/java/com/example/dondon/recyclerview/AsyncNetworkTask.java
@@ -0,0 +1,18 @@
+package com.example.dondon.recyclerview;
+
+import android.os.AsyncTask;
+
+public class AsyncNetworkTask extends AsyncTask<String,Void,String>{
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
+
+
+}
